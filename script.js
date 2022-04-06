5e27f18e3e2bebd02e5f48ddc22bb067de12e240
Parker Rudd <parkerbrudd@gmail.com>
2022-04-05 21:46:57 -0600

    created index.html script.js and style.css, and styled some of the page

---
+++ script.js
@@ -0,0 +1,13 @@
+// const navbar = document.querySelector('.nav')
+// const scrollDown = document.querySelector('.scrollDown')
+// let lastScrollY = window.scrollY; 
+
+// window.addEventListener("scroll", () =>  {
+//     if (lastScrollY < window.scrollY) {
+//         navbar.classList.add('nav-hide')
+//     } 
+//     else {
+//         navbar.classList.remove('nav-hide')
+//     } 
+//     lastScrollY = window.scrollY
+// })
