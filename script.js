fbf13a55f9f428c506c3d8f4df5a458111fe480e
Parker Rudd <parkerbrudd@gmail.com>
2022-04-16 09:50:20 -0600

    added smooth scrolling and fade out navbar

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-// let lastScrollTop = 0; 
-// const navbar = document.getElementById('navbar'); 
-// window.addEventListener('scroll', function() {
-//     let scrollTop = window.pageYOffset || document.documentElement.scrollTop; 
-//     if (scrollTop > lastScrollTop) {
-//         navbar.style.top = '-80px'; 
-//     } else {
-//         navbar.style.top = '0'; 
-//     }
-//     lastScrollTop = scrollTop; 
-// })
+let lastScrollTop = 0; 
+const navbar = document.getElementById('navbar'); 
+window.addEventListener('scroll', function() {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop; 
+    if (scrollTop > lastScrollTop) {
+        navbar.classList.add('hide-nav')
+    } else {
+        navbar.classList.remove('hide-nav') 
+    }
+    lastScrollTop = scrollTop; 
+})
