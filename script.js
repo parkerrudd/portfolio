18c0fbf2c77f01f4b64cc2a4a0b9b53adc64e4c9
Parker Rudd <parkerbrudd@gmail.com>
2022-04-19 16:27:50 -0600

    added modal for contact, still need to figure out formsubmit setup

--- script.js
+++ script.js
@@ -3,9 +3,25 @@ const navbar = document.getElementById('navbar');
 window.addEventListener('scroll', function() {
     let scrollTop = window.pageYOffset || document.documentElement.scrollTop; 
     if (scrollTop > lastScrollTop) {
-        navbar.classList.add('hide-nav')
+        navbar.classList.add('hide-nav'); 
     } else {
-        navbar.classList.remove('hide-nav') 
+        navbar.classList.remove('hide-nav');  
     }
     lastScrollTop = scrollTop; 
+})
+
+const open = document.getElementById('contact'); 
+const modalContainer = document.getElementById('modal_container'); 
+const close = document.getElementById('close'); 
+let timesClicked = 0; 
+open.addEventListener('click', () => {
+    timesClicked++; 
+    if (timesClicked % 2 !== 0) {
+        modalContainer.classList.add('show'); 
+    } else {
+        modalContainer.classList.remove('show'); 
+    }
+})
+close.addEventListener('click', () => {
+    modalContainer.classList.remove('show'); 
 })
